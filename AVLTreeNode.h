54518a82a47ed6c67c5de5d186d7bd444c7a7014
snirg <snirgreen@campus.technion.ac.il>
2020-04-30 19:01:32 +0300

    init commit

--- AVLTreeNode.h
+++ AVLTreeNode.h
@@ -5,8 +5,16 @@
 #ifndef WET1_AVLTREENODE_H
 #define WET1_AVLTREENODE_H
 
-
+template <class T>
 class AVLTreeNode {
+private:
+    int key;
+    T info;
+    AVLTreeNode *left;
+    AVLTreeNode *right;
+    AVLTreeNode *parent;
+public:
+    AVLTreeNode(int k, T i, AVLTreeNode *p) : key(k), info(i), left(nullptr), right(nullptr), parent(p);
 
 };
 
