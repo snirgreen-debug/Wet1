80537d8f0b3c76234a9fa271141c2913367fdf7e
snirg <snirgreen@campus.technion.ac.il>
2020-05-01 17:01:10 +0300

    * bugs fix. * deleted unnecessary AVLTreeNode cpp file. * adding nodes methods was added.

--- AVLTreeNode.h
+++ AVLTreeNode.h
@@ -5,14 +5,14 @@
 #ifndef WET1_AVLTREENODE_H
 #define WET1_AVLTREENODE_H
 
-enum status{
+enum status {
     ERROR_ALLOCATION,
     INVALID_INPUT,
     FAILURE,
     SUCCESS
 };
 
-template <class T>
+template<class T>
 class AVLTreeNode {
 private:
     int key;
@@ -26,20 +26,41 @@ private:
 //    void LR();
 //    void RL();
 public:
-    AVLTreeNode(int k, T i, AVLTreeNode *p);
-    ~AVLTreeNode();
+    AVLTreeNode(int k, T i, AVLTreeNode *p) : key(k), info(i), left(nullptr), right(nullptr), parent(p) {}
 
-//    status addLeftNode(int key, T info);
-//    status addRightNode(int key, T info);
+    ~AVLTreeNode() = default;
+
+    status addLeftNode(int k, T i) {
+        auto *newNode = new AVLTreeNode<T>(k, i, this);
+        this->left = newNode;
+        return status::SUCCESS;
+    }
+
+    status addRightNode(int k, T i) {
+        auto *newNode = new AVLTreeNode<T>(k, i, this);
+        this->right = newNode;
+        return status::SUCCESS;
+    }
 //    status deleteLeftNode(int key);
 //    status deleteRightNode(int key);
 
-    int calcHeight(AVLTreeNode node);
-    int calcBalance(AVLTreeNode node);
+    int calcHeight() {
+        if (!this) { return -1; }
+        int h_l = this->left->calcHeight();
+        int h_r = this->right->calcHeight();
+        int h_max = (h_l >= h_r) ? h_l : h_r;
+        return (1 + h_max);
+    }
+
+    int calcBalance() {
+        if (!this) { return 0; }
+        int left = this->left->calcHeight();
+        int right = this->right->calcHeight();
+        return (left - right);
+    }
 
 //    status preOrderIteration();
 
 };
-
 
 #endif //WET1_AVLTREENODE_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "AVLTreeNode.h"
+
+typedef AVLTreeNode<int> t;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x = 0;
+    t node(1, 2, nullptr);
+    node.addLeftNode(0, 8);
+
+    x = node.calcBalance();
+
+    std::cout << "node balance = " << x << std::endl;
     return 0;
 }
