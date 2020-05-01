f2d585e14ddb9b1546e90e66cece19206caf8ff3
snirg <snirgreen@campus.technion.ac.il>
2020-05-01 15:03:33 +0300

    added height and balance calculation

--- AVLTreeNode.cpp
+++ AVLTreeNode.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "AVLTreeNode.h"
+
+template<class T>
+AVLTreeNode<T>::AVLTreeNode(int k, T i, AVLTreeNode *p) : key(k), info(i), left(nullptr), right(nullptr), parent(p) {}
+
+template<class T>
+int AVLTreeNode<T>::calcHeight(AVLTreeNode node) {
+    if (!node) { return -1; }
+    return (1 + AVLTreeNode<T>::calcHeight(node.left) + AVLTreeNode<T>::calcHeight(node.right));
+}
+
+template<class T>
+int AVLTreeNode<T>::calcBalance(AVLTreeNode node) {
+    if (!node) { return -1; }
+    return (calcHeight(node.left) - calcHeight(node.right));
+}
--- AVLTreeNode.h
+++ AVLTreeNode.h
@@ -5,6 +5,13 @@
 #ifndef WET1_AVLTREENODE_H
 #define WET1_AVLTREENODE_H
 
+enum status{
+    ERROR_ALLOCATION,
+    INVALID_INPUT,
+    FAILURE,
+    SUCCESS
+};
+
 template <class T>
 class AVLTreeNode {
 private:
@@ -13,8 +20,24 @@ private:
     AVLTreeNode *left;
     AVLTreeNode *right;
     AVLTreeNode *parent;
+
+//    void LL();
+//    void RR();
+//    void LR();
+//    void RL();
 public:
-    AVLTreeNode(int k, T i, AVLTreeNode *p) : key(k), info(i), left(nullptr), right(nullptr), parent(p);
+    AVLTreeNode(int k, T i, AVLTreeNode *p);
+    ~AVLTreeNode();
+
+//    status addLeftNode(int key, T info);
+//    status addRightNode(int key, T info);
+//    status deleteLeftNode(int key);
+//    status deleteRightNode(int key);
+
+    int calcHeight(AVLTreeNode node);
+    int calcBalance(AVLTreeNode node);
+
+//    status preOrderIteration();
 
 };
 
