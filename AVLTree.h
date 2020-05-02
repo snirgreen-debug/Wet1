32df9d14a8af195f41b8d8ca4ed7cadf224cbcad
snirg <snirgreen@campus.technion.ac.il>
2020-05-03 02:54:51 +0300

    * Bug fix. * Added deleteNode function

--- AVLTree.h
+++ AVLTree.h
@@ -55,8 +55,8 @@ public:
         B->left = A->right;
         A->right = B;
 
-        if (B->parent) { A->parent = B->parent; }
         if (B->left) { B->left->parent = B; }
+        A->parent = B->parent;
         B->parent = A;
 
         A->height = max(getHeight(A->left), getHeight(A->right)) + 1;
@@ -70,8 +70,8 @@ public:
         B->right = A->left;
         A->left = B;
 
-        if (B->parent) { A->parent = B->parent; }
         if (B->right) { B->right->parent = B; }
+        A->parent = B->parent;
         B->parent = A;
 
         A->height = max(getHeight(A->left), getHeight(A->right)) + 1;
@@ -96,7 +96,7 @@ public:
         int nodeBalance = this->calcBalance(node);
         if (nodeBalance > 1) {
             int leftBalance = this->calcBalance(node->left);
-            if (leftBalance >= 0) {//todo: in the videos >=0 but in the internet >0
+            if (leftBalance >= 0) {//todo: in the videos >=0 but in the internet > 0
                 node = LL(node);
             } else {
                 node = LR(node);
@@ -144,23 +144,111 @@ public:
         return currentNode;
     }
 
+    Node<T> *insertNode(Node<T> *currentNode, Node<T> *newNode, Node<T> *parent = nullptr) {
+        if (!currentNode) {
+            return newNode;
+        }
+        if (newNode->key < currentNode->key) {
+            currentNode->left = insertEmptyNode(currentNode->left, newNode->key, currentNode);
+        }
+        if (newNode->key > currentNode->key) {
+            currentNode->right = insertEmptyNode(currentNode->right, newNode->key, currentNode);
+        }
+        if (newNode->key == currentNode->key) {
+            return currentNode;
+        }
+
+        currentNode = balanceNode(currentNode);
+        currentNode->height = max(getHeight(currentNode->left), getHeight(currentNode->right)) + 1;
+
+        return currentNode;
+    }
+
     Node<T> *getNodeByKey_aux(Node<T> *node, int key) {
-        if(!node){return nullptr;}
-        if(node->key == key){ return node;}
+        if (!node) { return nullptr; }
+        if (node->key == key) { return node; }
         Node<T> *leftNode = getNodeByKey_aux(node->left, key);
         Node<T> *rightNode = getNodeByKey_aux(node->right, key);
-        if(leftNode){
+        if (leftNode) {
             return leftNode;
         }
-        if(rightNode){
+        if (rightNode) {
             return rightNode;
         }
 
         return nullptr;
     }
 
-    Node<T> *getNodeByKey(int key){
+    Node<T> *getNodeByKey(int key) {
         return this->getNodeByKey_aux(this->root, key);
+    }
+
+    Node<T> *findFollowingNode_aux(Node<T> *node) {
+        if(!node->left){ return node;}
+        return findFollowingNode_aux(node->left);
+    }
+
+    Node<T> *findFollowingNode(Node<T> *node) {
+        if (!node) { return nullptr; }
+        if (!node->right) { return nullptr; }
+        return findFollowingNode_aux(node->right);
+    }
+
+    Node<T> *deleteNode(Node<T> *currentNode, int key, Node<T> *parent = nullptr) {
+        if (!currentNode) { return currentNode; }
+        Node<T> *retNode = nullptr;
+
+        if (key < currentNode->key) {
+            currentNode->left = deleteNode(currentNode->left, key, currentNode);
+        }
+        if (key > currentNode->key) {
+            currentNode->right = deleteNode(currentNode->right, key, currentNode);
+        }
+        if (key == currentNode->key) {
+            if (!currentNode->left && !currentNode->right) {
+//                delete currentNode->info;
+                delete currentNode;
+                return nullptr;
+            }
+            if (!currentNode->left) {
+                retNode = currentNode->right;
+//                delete currentNode->info;
+                delete currentNode;
+                return retNode;
+            }
+            if (!currentNode->right) {
+                retNode = currentNode->left;
+//                delete currentNode->info;
+                delete currentNode;
+                return retNode;
+            }
+            if (currentNode->left && currentNode->right) {
+                Node<T> *followingNode = this->findFollowingNode(currentNode);
+                Node<T> *p = followingNode->parent;
+                Node<T> *r = followingNode->right;
+
+                followingNode->parent = currentNode->parent;
+                followingNode->right = currentNode->right;
+                followingNode->left = currentNode->left;
+
+                currentNode->parent = p;
+                currentNode->right = r;
+                currentNode->left = nullptr;
+
+                p->left = currentNode;
+                followingNode->left->parent = followingNode;
+                followingNode->right->parent = followingNode;
+
+                currentNode = followingNode;
+
+                currentNode->right = deleteNode(currentNode->right, key, currentNode->parent);
+            }
+        }
+
+        currentNode = balanceNode(currentNode);
+        currentNode->height = max(getHeight(currentNode->left), getHeight(currentNode->right)) + 1;
+
+        return currentNode;
     }
 };
 
